de3a4b490f6ff4c79539e28ec404326c4fcc0326
RajkrishnaRana <rajkrishnaranastudy@gmail.com>
2024-04-08 03:17:03 +0530

    removing unused data

--- App.jsx
+++ App.jsx
@@ -3,17 +3,6 @@ import React, {useState, useEffect} from 'react';
 import DropdownComponent from './components/DropdownComponent';
 import countryData from 'countrycitystatejson';
 
-const data = [
-  {label: 'Item 1', value: '1'},
-  {label: 'Item 2', value: '2'},
-  {label: 'Item 3', value: '3'},
-  {label: 'Item 4', value: '4'},
-  {label: 'Item 5', value: '5'},
-  {label: 'Item 6', value: '6'},
-  {label: 'Item 7', value: '7'},
-  {label: 'Item 8', value: '8'},
-];
-
 const App = () => {
   const data = countryData.getAll();
   const [allCountryData, setAllCountryData] = useState([]);
